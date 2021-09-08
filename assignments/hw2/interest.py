41ea5abacf71505a0599384a5a34be731389d0fa
Steve Ford <styxstyx101@gmail.com>
2021-09-08 10:29:56 -0400

    Homework: "Interest Problem" submitted

--- assignments/hw2/interest.py
+++ assignments/hw2/interest.py
@@ -1,10 +1,23 @@
-"""Calculates Interest"""
+"""
+Name: Steve Ford
+interest.py
+
+Certificate of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
 
 
-def main(ann_interest, cyc_length, net_bal, payment_made, day_made):
-    interest_charge = (((net_bal*cyc_length - payment_made * (cyc_length - day_made))/cyc_length)*(ann_interest/1200))
+def main():
+
+    annual_interest = eval((input("annual interest ")))
+    cyc_length = eval(input("Days in month "))
+    net_bal = eval(input("Previous Balance "))
+    payment_made = eval(input("Payment made "))
+    day_made = eval(input("Day of Payment "))
+    sum_charge = ((net_bal*cyc_length - payment_made * (cyc_length - day_made))/cyc_length)
+    interest_charge = sum_charge * (annual_interest/1200)
+    interest_charge = round(interest_charge, 2)
     print(interest_charge)
-    return interest_charge
 
 
-main(15.84, 31, 850, 400, 20)
+# main()
